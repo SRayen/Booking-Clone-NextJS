53f4a1f4653363e4a707110c1626b3f66a807fdb
Selmen Rayen <selmanerayene95@gmail.com>
2023-12-14 02:09:32 +0100

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Booking.com clone',
+  description: 'Booking.com created by SRayen',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
