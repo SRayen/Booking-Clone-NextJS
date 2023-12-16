24907ec392e6c5b93f228b0f06117d740f2a7f95
Selmen Rayen <selmanerayene95@gmail.com>
2023-12-16 20:38:31 +0100

    Build header component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,26 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/Header";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Booking.com clone',
-  description: 'Booking.com created by SRayen',
-}
+  title: "Booking.com clone",
+  description: "Booking.com created by SRayen",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
